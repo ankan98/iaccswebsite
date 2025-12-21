ab6d63ff31a00c1da723b5bed630d81b6334fc89
subhankar86 <sprasadcm2016@gmail.com>
2025-12-21 21:38:49 +0530

    added images in about page and fixed responsive issues

--- iaccs/src/app/(pages)/about-us/page.tsx
+++ iaccs/src/app/(pages)/about-us/page.tsx
@@ -6,15 +6,15 @@ export default function About() {
   <>
   <header className="relative w-full flex flex-col gap-48 bg-transparent bg-[url(/assets/images/banner-img.png)] bg-cover bg-center py-32 mt-20 bg-gray-500">
       <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="container mx-auto px-[110px] relative z-[1]">
+        <div className="container mx-auto xl:px-[110px] md:px-20 px-5 relative z-[1]">
             <div className="w-full max-w-[642px] flex flex-col gap-12">
-                <h1 className="w-full max-w-[640px] font-bold text-white text-[64px] tracking-[0] leading-[76.8px]">About Us</h1>
+                <h1 className="w-full max-w-[640px] font-bold text-white xl:text-[64px] md:text-4xl text-3xl tracking-[0] xl:leading-[76.8px]">About Us</h1>
                 {/* <p className="text-lg text-white">Join us in providing life-saving medical care to communities without access to the basic healthcare they need.</p> */}
             </div>
         </div>
     </header>
     
-    <section className="w-full flex flex-col gap-16 py-16 px-[110px]">
+    <section className="w-full flex flex-col gap-16 py-16 xl:px-[110px] md:px-20 px-5">
         <div className="w-full flex gap-[82px]">
             <div className="flex-1 flex flex-col gap-8 relative">
                 {/* <div className="flex items-center gap-8">
@@ -53,7 +53,7 @@ export default function About() {
         </div>
     </section>
 
-    {/* <section className="w-full flex flex-col gap-16 py-16 px-[110px] bg-yellow-500">
+    {/* <section className="w-full flex flex-col gap-16 py-16 xl:px-[110px] md:px-20 px-5 bg-yellow-500">
         <div className="w-full flex gap-[82px]">
             <div className="max-w-6xl mx-auto px-6 grid md:grid-cols-2 gap-10 items-center">
 
@@ -91,42 +91,42 @@ export default function About() {
         </div>
     </section> */}
 
-    <section className="w-full flex flex-col gap-16 py-16 px-[110px]">
+    <section className="w-full flex flex-col gap-16 py-16 xl:px-[110px] md:px-20 px-5">
         <div className="w-full flex gap-[82px]">
             <div className="flex-1 flex flex-col gap-8 relative text-center">
                 <h2 className="font-bold text-primary-text xl:text-[48px] md:text-3xl text-2xl">Our Governing Members</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
                     <div className="bg-white p-6 rounded-xl text-center">
                         <img
-                        src="assets/images/male-placeholder.jpg"
+                        src="assets/images/bapan-sarkar.jpg"
                         alt="team member"
                         className="w-48 h-48 mx-auto rounded-xl object-cover border border-gray-200"
                         />
-                        <h3 className="text-xl font-semibold mt-4">BAPAN SARKAR</h3>
-                        <p className="text-gray-600">PRESIDENT</p>
-                        <p>MSc. CCS </p>
+                        <h3 className="text-xl font-semibold uppercase mt-4">Bapan Sarkar</h3>
+                        <p className="text-gray-600">President</p>
+                        <p>M.sc CCST</p>
                     </div>
 
                     <div className="bg-white p-6 rounded-xl text-center">
                         <img
-                        src="assets/images/male-placeholder.jpg"
+                        src="assets/images/atri-banerjee.jpg"
                         alt="team member"
                         className="w-48 h-48 mx-auto rounded-xl object-cover border border-gray-200"
                         />
-                        <h3 className="text-xl font-semibold mt-4">ATRI BANERJEE</h3>
-                        <p className="text-gray-600">GENERAL SECRETARY</p>
-                        <p>BSc. CCT</p>
+                        <h3 className="text-xl font-semibold uppercase mt-4">Atri Banerjee </h3>
+                        <p className="text-gray-600">General Secretary</p>
+                        <p>B.sc CCT</p>
                     </div>
 
                     <div className="bg-white p-6 rounded-xl text-center">
                         <img
-                        src="assets/images/female-placeholder.jpg"
+                        src="assets/images/trina-ghorai.jpg"
                         alt="team member"
                         className="w-48 h-48 mx-auto rounded-xl object-cover border border-gray-200"
                         />
-                        <h3 className="text-xl font-semibold mt-4">TRINA GHORAI </h3>
-                        <p className="text-gray-600">VICE PRESIDENT</p>
-                        <p>BSc. CCT</p>
+                        <h3 className="text-xl font-semibold uppercase mt-4">Trina Ghorai</h3>
+                        <p className="text-gray-600">Vice President</p>
+                        <p>M.sc CCST</p>
                     </div>
 
                 </div>
